336110bc0e71e36aaffb687b34691c3fed675a1b
John Paredes <john.paredes@gmail.com>
2013-09-15 21:23:45 -0700

    Added unique validation for email and require name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create }
+  validates :name, presence: true
 end
